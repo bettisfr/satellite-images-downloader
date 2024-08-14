ede2cdd4897947af1d732fb369b404c3aca5ea54
francesco <francesco.betti.sorbelli@gmail.com>
2024-08-14 11:56:13 +0200

    fix crop

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import ee
 import os
 import requests
+import random
 from geopy.distance import geodesic
 from geopy.point import Point
 from PIL import Image, ImageChops
 
-
 ####### Parameters
 # Resolution in meters [minimum 0.6]
-scale = 2
+scale = 0.6
 
 # Radius in meters of a circle that defines the region of interest
-buffer_radius = 2000
+buffer_radius = 800
 
 # Latitude of the bottom-left corner of the area
 latitude_bottom_left = 37.910715173463
@@ -20,10 +20,10 @@ latitude_bottom_left = 37.910715173463
 longitude_bottom_left = -91.77332884614303
 
 # Length of the area in meters
-area_length = 8000
+area_length = 20000
 
 # Height of the area in meters
-area_height = 8000
+area_height = 20000
 
 
 def crop_black_borders(image_path):
@@ -88,7 +88,7 @@ def download_image(latitude, longitude, pair):
     if not os.path.exists(out_folder):
         os.makedirs(out_folder)
 
-    output_path = f'{out_folder}/naip_{x}_{y}.tif'
+    output_path = f'{out_folder}/naip_{x}_{y}__{latitude}_{longitude}.tif'
     with open(output_path, 'wb') as file:
         file.write(response.content)
 
@@ -98,7 +98,7 @@ def download_image(latitude, longitude, pair):
     crop_black_borders(output_path)
 
 
-def batch():
+def download_satellite_images():
     # Authentication and initialization
     ee.Authenticate()
     ee.Initialize(project='ee-francescobettisorbelli')
@@ -108,6 +108,8 @@ def batch():
 
     # Create a starting point as a geopy Point object
     start_point = Point(latitude_bottom_left, longitude_bottom_left)
+
+    print(f"Grid formed by {num_rows} rows and {num_columns} columns")
 
     for x in range(num_rows):
         for y in range(num_columns):
@@ -128,5 +130,69 @@ def batch():
             download_image(new_latitude, new_longitude, (x, y))
 
 
+def manipulate_image(image_path, output_path, crop_size=500):
+    with Image.open(image_path) as img:
+        # List of allowed rotation angles
+        angles = [0, 90, 180, 270]
+
+        # Randomly select one of the allowed angles
+        angle = random.choice(angles)
+        rotated_img = img.rotate(angle, expand=True)
+
+        # Image dimensions after rotation
+        width, height = rotated_img.size
+
+        # Determine the cropping box
+        if width > crop_size and height > crop_size:
+            left = random.randint(0, width - crop_size)
+            top = random.randint(0, height - crop_size)
+        else:
+            # Center crop if the image is smaller than crop_size x crop_size
+            left = max((width - crop_size) // 2, 0)
+            top = max((height - crop_size) // 2, 0)
+
+        right = left + crop_size
+        bottom = top + crop_size
+
+        # Crop the image
+        cropped_img = rotated_img.crop((left, top, right, bottom))
+
+        # Save the manipulated image
+        cropped_img.save(output_path)
+
+
+def create_random_uav_images():
+    out_folder = f'out/size{buffer_radius}_res{scale}'
+    out_uav_folder = f'out_uav/size{buffer_radius}_res{scale}'
+
+    os.makedirs(out_uav_folder, exist_ok=True)
+
+    if scale != 0.6:
+        print("For this task, please use scale=0.6")
+        exit(-1)
+
+    for root, _, files in os.walk(out_folder):
+        for file in files:
+            parts = file.split('_')
+            x = parts[1]
+            y = parts[2]
+            latitude = parts[4]
+            longitude = parts[5].split('.tif')[0]
+
+            # Print the extracted values
+            print(f"x: {x}, y: {y}, latitude: {latitude}, longitude: {longitude}")
+
+            # Full path to the input image
+            file_path = os.path.join(root, file)
+
+            # Define the output path
+            output_path = os.path.join(out_uav_folder, file)
+
+            # Call the image manipulation function
+            manipulate_image(file_path, output_path, 1000)
+
+
 if __name__ == "__main__":
-    batch()
+    # download_satellite_images()
+
+    create_random_uav_images()
